4b79dcf96d31e5f8eb50068b5b5287d808b99bad
tangyiyang <sleepers.tang@gmail.com>
2016-03-23 12:02:28 +0800

    code clean up

--- src/app/layers/game_layer.lua
+++ src/app/layers/game_layer.lua
@@ -6,41 +6,33 @@ local ACTOR_CAEMRA_FLAG = 2
 function game_layer:ctor( )
 	local actor = display.newSprite("#ghost_1.png")
 	actor:setPosition(display.width/2, display.height/2)
-	-- actor:setPosition(0, 0)
+	self.actor = actor
 	self:addChild(actor)
 
 	local camera = cc.Camera:createOrthographic(display.width, display.height, 0, 1)
 	camera:setCameraFlag(ACTOR_CAEMRA_FLAG)
 	self:addChild(camera)
+    self.camera = camera
 
-	actor:setCameraMask(ACTOR_CAEMRA_FLAG)
-
-	-- camera:setPosition(cc.p(200, 200))
 	self:setNodeEventEnabled(true)
 	self:setTouchEnabled(true)
     self:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
     	local x, y = event.x, event.y
-    	local cx, cy = camera:getPosition()
     	if event.name == "began" then 
-    		-- print(string.format("click x, y = %.2f, %.2f", x, y))
-    		local p = self:convertToWorldSpace(cc.p(x, y))
 
+    		local p = self:convertToWorldSpace(cc.p(x, y))
     		local tx, ty = p.x + self.camera:getPositionX(), p.y + self.camera:getPositionY()
 
-    		print(string.format("click = %.2f, %.2f, p = %.2f, %.2f, tx, ty = %.2f, %.2f",x, y, p.x, p.y, tx, ty))
-    		actor:setPosition(tx, ty)
-    		-- local world_x, world_y = p.x + cx, p.y + cy
-    		-- print("node, x, y = ", world_x, world_y)
+    		-- print(string.format("click = %.2f, %.2f, p = %.2f, %.2f, tx, ty = %.2f, %.2f",x, y, p.x, p.y, tx, ty))
+    		actor:runAction(cc.MoveTo:create(0.5, cc.p(tx, ty)))
     	end
 
     	return true
     end)
 
     self:setContentSize(display.width*2, display.height*2)
-    self.camera = camera
-    self.actor = actor
 
-    -- self.camera:runAction(cc.Follow:create(actor))
+    actor:setCameraMask(ACTOR_CAEMRA_FLAG)
 end
 
 function game_layer:get_camera()
@@ -53,9 +45,14 @@ end
 
 function game_layer:update()
 	local ox, oy = self.actor:getPosition()
-	self.camera:setPosition(ox - display.width/2, oy - display.height/2)
-	-- ox,oy = self.camera:getPosition()
-	-- self.camera:setPosition(ox + 1, oy)
+	local cx, cy = ox - display.width/2, oy - display.height/2
+	cx = cx < 0 and 0 or cx
+	cy = cy < 0 and 0 or cy
+
+	cx = cx > display.width and display.width or cx
+	cy = cy > display.height and display.height or cy
+	
+	self.camera:setPosition(cx, cy)
 end
 
 return game_layer
